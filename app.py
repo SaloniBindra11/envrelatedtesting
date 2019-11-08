eb5f38d166c3608d20714780ba96994c82964ef1
SaloniBindra11 <55702373+SaloniBindra11@users.noreply.github.com>
2019-11-08 17:32:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
 
 
-import openshift as oc
-
-if __name__ == '__main__':
-    with oc.tracking() as tracker:
-        try:
-            print('Current project: {}'.format(oc.get_fields()))
-            print('Current user: {}'.format(oc.whoami()))
-        except:
-            print('Error acquire details about project/user')
-
-
+print("hello")
+print(process.env.fields)
